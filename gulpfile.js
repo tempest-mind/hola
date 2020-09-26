b13b980dac455dc3bead46120541b8326dd05d48
tempest-mind <71837023+tempest-mind@users.noreply.github.com>
2020-09-26 09:03:21 -0700

    Adding eslint cfg and gulp buld step...

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 
-const { series, prallel, src, dest, watch } = require('gulp');
+const { series, parallel, src, dest, watch } = require('gulp');
+const del = require('del');
+
+let wd = ''; // Workding directory
+
+//console.log('process.argv', process.argv);
 
 function defaultTask(cb) {
-  "use strict";  
   // place code for your default task here
   cb();
 }
 
+function clean() {
+  return del(wd + 'dist');
+}
+
+function move() {
+  return src(wd + 'src/**')
+    .pipe((dest(wd + 'dist')));
+}
+
+
 exports.default = defaultTask;
+exports.clean = clean;
+exports.move = move;
+
+exports.build = series(clean, move);

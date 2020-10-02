dd96cadf75fb3c22379bc382a6446ba3e6d8d726
tempest-mind <71837023+tempest-mind@users.noreply.github.com>
2020-10-02 10:53:01 -0700

    Changing function status code.

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ exports.handler = async event => {
   // Get the 'state' and redirect.
   const redirect = event.queryStringParameters.state || 'https://stupefied-shirley-45b0a3.netlify.app/';
   return {
-    statusCode: 303,
+    statusCode: 302,
     Location: redirect,
     body: `Hello ${redirect}`
   };

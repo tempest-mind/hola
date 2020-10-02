f418bba7de56c52bd5df569754da5c4482e47c9e
tempest-mind <71837023+tempest-mind@users.noreply.github.com>
2020-10-02 16:08:45 -0700

    Updates to the serverless function.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 exports.handler = async event => {
   const params = event.queryStringParameters;
-  const state = params.state || 'https://stupefied-shirley-45b0a3.netlify.app/';
+  const state = params.state || 'https://stupefied-shirley-45b0a3.netlify.app/app.html';
+
+  console.log('params.state', params.state);
+  console.log('state', state);
 
   return {
     statusCode: 302,
